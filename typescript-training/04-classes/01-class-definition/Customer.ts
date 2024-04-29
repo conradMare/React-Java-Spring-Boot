690d9368b311f380d5b1fb3626575c2bd6c0f3c2
conradMare <conradmare4@gmail.com>
2024-04-29 13:10:22 +0200

    2024/04/29(pm)
    01-class-defintion - S06.35_Creating Classes - Write Some Code

---
+++ typescript-training/04-classes/01-class-definition/Customer.ts
@@ -0,0 +1,14 @@
+class Customer {
+
+    firstName: string;
+    lastName: string;
+}
+
+// Create an instance
+let myCustomer = new Customer();
+
+myCustomer.firstName = "Martin";
+myCustomer.lastName = "Dixon";
+
+console.log(myCustomer.firstName);
+console.log(myCustomer.lastName);
