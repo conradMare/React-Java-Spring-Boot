84ee0b6abb01e2291137e260d68de1d389110ef1
conradMare <conradmare4@gmail.com>
2024-04-29 13:52:53 +0200

    2024/04/29(pm)
    02-access-modifiers - S06.37_Define Accessors - Write Some Code

--- typescript-training/04-classes/01-class-definition/Customer.ts
+++ typescript-training/04-classes/01-class-definition/Customer.ts
@@ -2,13 +2,25 @@ class Customer {
 
     firstName: string;
     lastName: string;
+
+    // Define a constructor
+    constructor(theFirst: string, theLast: string) {
+        this.firstName = theFirst;
+        this.lastName = theLast;
+    }
 }
 
 // Create an instance
-let myCustomer = new Customer();
+// let myCustomer = new Customer();
 
-myCustomer.firstName = "Martin";
-myCustomer.lastName = "Dixon";
+// myCustomer.firstName = "Martin";
+// myCustomer.lastName = "Dixon";
+
+// console.log(myCustomer.firstName);
+// console.log(myCustomer.lastName);
+
+// Use the constructor
+let myCustomer = new Customer("Martin", "Dixon");
 
 console.log(myCustomer.firstName);
 console.log(myCustomer.lastName);
---
+++ typescript-training/04-classes/02-access-modfiers/Customer.ts
@@ -0,0 +1,32 @@
+class Customer {
+
+    private _firstName: string;
+    private _lastName: string;
+
+    constructor(theFirst: string, theLast: string) {
+        this._firstName = theFirst;
+        this._lastName = theLast;
+    }
+
+    // Getters and Setters:
+    public get firstName(): string {
+        return this._firstName;
+    }
+
+    public set firstName(value: string) {
+        this._firstName = value;
+    }
+
+    public get lastName(): string {
+        return this._lastName;
+    }
+
+    public set lastName(value: string) {
+        this._lastName = value;
+    }
+}
+
+let myCustomer1 = new Customer("Martin", "Dixon");
+
+console.log(myCustomer1.firstName);
+console.log(myCustomer1.lastName);
