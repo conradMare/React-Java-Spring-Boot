781d0fd1189d28854344a0b49dfcb33da16e8fd7
conradMare <conradmare4@gmail.com>
2024-04-29 08:52:30 +0200

    2024/04/29(am)
    02-variables - S05.30_Defining variables with some code

---
+++ typescript-training/02-variables/sample-type.js
@@ -0,0 +1,13 @@
+var found = true;
+var grade = 88.6;
+var firstName = "Anup";
+var lastName = 'Kumar';
+// let's try and break it
+// found = 0;
+// grade = "A";
+// firstName = false;
+console.log(found);
+console.log("The grade is " + grade);
+console.log("Hi " + firstName + " " + lastName);
+// template strings:
+console.log("Hi ".concat(lastName, " + ").concat(firstName));
