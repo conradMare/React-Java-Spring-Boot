a3d945433451748ddbad72304ddc1bee8cb9ecc0
conradMare <conradmare4@gmail.com>
2024-05-01 09:54:04 +0200

    2024/05/01(am)
    react-library - S10.67_React Project - Home Page - Heroes Component Part 2

--- library-app/03-frontend/react-library/src/App.tsx
+++ library-app/03-frontend/react-library/src/App.tsx
@@ -3,13 +3,15 @@ import './App.css';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 import { ExploreTopBooks } from './layouts/HomePage/ExploreTopBooks';
 import { Carousel } from './layouts/HomePage/Carousel';
+import { Heros } from './layouts/HomePage/Heros';
 
 function App() {
   return (
     <div>
-      <Navbar />
-      <ExploreTopBooks />
-      <Carousel />
+      <Navbar/>
+      <ExploreTopBooks/>
+      <Carousel/>
+      <Heros/>
     </div>
   );
 }
