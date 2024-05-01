76b13795952f34faa0164c2d02d17dda6109ea20
conradMare <conradmare4@gmail.com>
2024-05-01 05:15:13 +0200

    2024/05/01(am)
    react-todos - S08.54_React and TypeScript - Part 4

--- react-todo-demo/react-todos/src/components/NewTodoForm.tsx
+++ react-todo-demo/react-todos/src/components/NewTodoForm.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-function NewTodoForm(props: {addTodo: Function}) {
+export const NewTodoForm: React.FC<{addTodo: Function}> = (props) => {
 
     const [description, setDescription] = useState('');
     const [assigned, setAssigned] = useState('');
@@ -45,5 +45,3 @@ function NewTodoForm(props: {addTodo: Function}) {
         </div>
     )
 }
-
-export default NewTodoForm
