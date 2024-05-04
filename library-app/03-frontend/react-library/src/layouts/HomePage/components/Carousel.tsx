7ecad4bad38826e524fe9384185b30e28cf5e800
conradMare <conradmare4@gmail.com>
2024-05-04 06:02:22 +0200

    2024/05/04(am)
    react-library - S15.110_React - Create Book Checkout Page Component

--- library-app/03-frontend/react-library/src/layouts/HomePage/components/Carousel.tsx
+++ library-app/03-frontend/react-library/src/layouts/HomePage/components/Carousel.tsx
@@ -1,7 +1,6 @@
 import { ReturnBook } from "./ReturnBook";
 import { useEffect, useState } from "react";
 import BookModel from "../../../models/BookModel";
-import { error } from "console";
 import { SpinnerLoading } from "../../Utils/SpinnerLoading";
 import { Link } from "react-router-dom";
 
@@ -20,7 +19,7 @@ export const Carousel = () => {
             const response = await fetch(url);
 
             if (!response.ok) {
-                throw new Error('Something went wrong');
+                throw new Error('Something went wrong!');
             }
 
             const responseJson = await response.json();
@@ -39,7 +38,7 @@ export const Carousel = () => {
                     copiesAvailable: responseData[key].copiesAvailable,
                     category: responseData[key].category,
                     img: responseData[key].img,
-                })
+                });
             }
 
             setBooks(loadedBooks);
@@ -53,72 +52,70 @@ export const Carousel = () => {
 
     if (isLoading) {
         return (
-            <div className="container m-5">
-                <SpinnerLoading />
-            </div>
+            <SpinnerLoading />
         )
     }
 
     if (httpError) {
         return (
-            <div className="caontainer m-5">
+            <div className='container m-5'>
                 <p>{httpError}</p>
             </div>
         )
     }
 
     return (
-        <div className="container mt-5" style={{ height: 550 }}>
-            <div className="homepage-carousel-title">
-                <h3>Find your next book.</h3>
+        <div className='container mt-5' style={{ height: 550 }}>
+            <div className='homepage-carousel-title'>
+                <h3>Find your next "I stayed up too late reading" book.</h3>
             </div>
-            <div className="carousel carousel-dark slide mt-5 d-none d-lg-block" id="carouselExampleControls"
-                data-bs-interval="false">
+            <div id='carouselExampleControls' className='carousel carousel-dark slide mt-5 
+                d-none d-lg-block' data-bs-interval='false'>
 
                 {/* Desktop */}
-                <div className="carousel-inner">
-                    <div className="carousel-item active">
-                        <div className="row d-flex justify-content-center align-items-center">
+                <div className='carousel-inner'>
+                    <div className='carousel-item active'>
+                        <div className='row d-flex justify-content-center align-items-center'>
                             {books.slice(0, 3).map(book => (
-                                < ReturnBook book={book} key={book.id} />
+                                <ReturnBook book={book} key={book.id} />
                             ))}
                         </div>
                     </div>
-                    <div className="carousel-item">
-                        <div className="row d-flex justify-content-center align-items-center">
+                    <div className='carousel-item'>
+                        <div className='row d-flex justify-content-center align-items-center'>
                             {books.slice(3, 6).map(book => (
-                                < ReturnBook book={book} key={book.id} />
+                                <ReturnBook book={book} key={book.id} />
                             ))}
                         </div>
                     </div>
-                    <div className="carousel-item">
-                        <div className="row d-flex justify-content-center align-items-center">
+                    <div className='carousel-item'>
+                        <div className='row d-flex justify-content-center align-items-center'>
                             {books.slice(6, 9).map(book => (
-                                < ReturnBook book={book} key={book.id} />
+                                <ReturnBook book={book} key={book.id} />
                             ))}
                         </div>
                     </div>
                 </div>
-                <button className="carousel-control-prev" type="button"
-                    data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span className="visually-hidden">Previous</span>
+                <button className='carousel-control-prev' type='button'
+                    data-bs-target='#carouselExampleControls' data-bs-slide='prev'>
+                    <span className='carousel-control-prev-icon' aria-hidden='true'></span>
+                    <span className='visually-hidden'>Previous</span>
                 </button>
-                <button className="carousel-control-next" type="button"
-                    data-bs-target="#carouselExampleControls" data-bs-slide="next">
-                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span className="visually-hidden">Next</span>
+                <button className='carousel-control-next' type='button'
+                    data-bs-target='#carouselExampleControls' data-bs-slide='next'>
+                    <span className='carousel-control-next-icon' aria-hidden='true'></span>
+                    <span className='visually-hidden'>Next</span>
                 </button>
             </div>
 
             {/* Mobile */}
-            <div className="d-lg-none mt-3">
-                <div className="row d-flex justify-content-center align-items-center">
+            <div className='d-lg-none mt-3'>
+                <div className='row d-flex justify-content-center align-items-center'>
                     <ReturnBook book={books[7]} key={books[7].id} />
                 </div>
             </div>
-            <div className="homepage-carousel-title mt-3">
-                <Link className="btn btn-outline-secondary btn-lg" to="/search">More</Link>
+            <div className='homepage-carousel-title mt-3'>
+                <Link className='btn btn-outline-secondary btn-lg' to='/search'>View More</Link>
             </div>
         </div>
     );
