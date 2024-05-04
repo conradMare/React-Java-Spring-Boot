0cc2c2809e01724d9c51af45588b2cb566ea696e
conradMare <conradmare4@gmail.com>
2024-05-04 04:54:25 +0200

    2024/05/04(am)
    react-library - S14.102_React Router - Installation

--- library-app/03-frontend/react-library/src/App.tsx
+++ library-app/03-frontend/react-library/src/App.tsx
@@ -4,13 +4,18 @@ import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 import { Footer } from './layouts/NavbarAndFooter/Footer';
 import { HomePage } from './layouts/HomePage/HomePage';
 import { SearchBooksPage } from './layouts/SearchBooksPage/SearchBooksPage';
+import { Route } from 'react-router-dom';
 
 export const App = () => {
   return (
     <div>
       <Navbar />
-      {/* <HomePage /> */}
+      <Route path='/'>
+        <HomePage />
+      </Route>
+      <Route path='/search'>
       <SearchBooksPage />
+      </Route>
       <Footer />
     </div>
   );
