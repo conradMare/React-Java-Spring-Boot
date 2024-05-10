a4fb9fb2a308c3664c8d08a60520d26d6b9e3494
conradMare <conradmare4@gmail.com>
2024-05-10 10:23:58 +0200

    2024/05/10(am)
    react-library-S26.201_React - History Page

---
+++ library-app/03-frontend/react-library/src/layouts/ShelfPage/components/HistoryPage.tsx
@@ -0,0 +1,48 @@
+import { useOktaAuth } from "@okta/okta-react";
+import { useEffect, useState } from "react";
+import HistoryModel from "../../../models/HistoryModels";
+import { SpinnerLoading } from "../../Utils/SpinnerLoading";
+
+export const HistoryPage = () => {
+
+    const { authState } = useOktaAuth();
+    const [isLoadingHistory, setIsLoadingHistory] = useState(true);
+    const [httpError, setHttpError] = useState(0);
+
+    // Histories
+    const [histories, setHistory] = useState<HistoryModel[]>([]);
+
+    // Pagination
+    const [currentPage, setCurrentPage] = useState(1);
+    const [totalPages, setTotalPages] = useState(0);
+
+    useEffect(() => {
+        const fetchUserHistory = async () => {
+
+        }
+        fetchUserHistory().catch((error: any) => {
+            setIsLoadingHistory(false);
+            setHttpError(error.message);
+        })
+    }, [authState, currentPage]);
+
+    if (isLoadingHistory) {
+        return (
+            <SpinnerLoading />
+        )
+    }
+
+    if (httpError) {
+        return (
+            <div className="container m-5">
+                <p>{httpError}</p>
+            </div>
+        );
+    }
+
+    const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
+
+    return (
+
+    );
+}
