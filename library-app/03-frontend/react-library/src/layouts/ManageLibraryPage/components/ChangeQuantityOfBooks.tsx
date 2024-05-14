a024999fca0ebb3773bf8b8f706c3b76b409c233
conradMare <conradmare4@gmail.com>
2024-05-14 10:07:41 +0200

    2024/05/14(am)
    react-library - S30.259_React - Implementation of Delete

--- library-app/03-frontend/react-library/src/layouts/ManageLibraryPage/components/ChangeQuantityOfBooks.tsx
+++ library-app/03-frontend/react-library/src/layouts/ManageLibraryPage/components/ChangeQuantityOfBooks.tsx
@@ -14,6 +14,7 @@ export const ChangeQuantityOfBooks = () => {
     const [booksPerPage] = useState(5);
     const [totalAmountOfBooks, setTotalAmountOfBooks] = useState(0);
     const [totalPages, setTotalPages] = useState(0);
+    const [bookDelete, setBookDelete] = useState(false);
 
     useEffect(() => {
         const fetchBooks = async () => {
@@ -54,7 +55,7 @@ export const ChangeQuantityOfBooks = () => {
             setIsLoading(false);
             setHttpError(error.message);
         })
-    }, [currentPage]);
+    }, [currentPage, bookDelete]);
 
     const indexOfLastBook: number = currentPage * booksPerPage;
     const indexOfFirstBook: number = indexOfLastBook - booksPerPage;
@@ -62,6 +63,8 @@ export const ChangeQuantityOfBooks = () => {
         booksPerPage * currentPage : totalAmountOfBooks;
 
     const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
+
+    const deleteBook = () => setBookDelete(!bookDelete);
 
     if (isLoading) {
         return (
@@ -88,7 +91,7 @@ export const ChangeQuantityOfBooks = () => {
                         {indexOfFirstBook + 1} to {lastItem} of {totalAmountOfBooks} items:
                     </p>
                     {books.map(book => (
-                        <ChangeQuantityOfBook book={book} key={book.id} />
+                        <ChangeQuantityOfBook book={book} key={book.id} deleteBook={deleteBook}/>
                     ))}
                 </>
                 :
